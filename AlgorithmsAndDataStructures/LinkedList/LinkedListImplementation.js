99af4c500f40744613f3811ff0d5971107c2f381
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2020-02-19 23:57:00 +0530

    Added remove() method to the Linked List implementations

--- AlgorithmsAndDataStructures/LinkedList/LinkedListImplementation.js
+++ AlgorithmsAndDataStructures/LinkedList/LinkedListImplementation.js
@@ -11,6 +11,9 @@ class LinkedList {
         this.length = 1;
     }
 
+    //Add a new node at the end of the linked list with the value provided
+    // Time complexity: O(1)
+    // Space complexity: O(1)
     append(value) {
         this.tail.next = {
             value: value,
@@ -22,17 +25,21 @@ class LinkedList {
         return this;
     }
 
+    //Add a new node at the start of the linked list with the value provided
+    // Time complexity: O(1)
+    // Space complexity: O(1)
     prepend(value) {
         this.head = {
             value: value,
             next: this.head
         }
-
         this.length++;
-
         return this;
     }
 
+    //Return the array with all the values in the linked list node for printing them onto the console
+    // Time complexity: O(n)
+    // Space complexity: O(n)
     printList() {
         const ar = [];
         let currentNode = this.head;
@@ -43,6 +50,9 @@ class LinkedList {
         return ar;
     }
 
+    //Inssert a node from the given index in the linked list
+    // Time complexity: O(n)
+    // Space complexity: O(1)
     insert(index,value) {
         if(index === 0) {
             this.prepend(value);
@@ -51,17 +61,13 @@ class LinkedList {
             this.append(value);
         }
         else if(index <= (this.length - 1) ) {
-            let currentNode = this.head;
-            for(let i=1; i< this.length -2; i++) {
-                if(index === i+1) {
-                    const newNode = {
-                        value: value,
-                        next: currentNode.next
-                    }
-                    currentNode.next = newNode;
-                }
-                currentNode = currentNode.next;
-            }    
+            const leader = this.traverseToIndex(index);
+            const newNode = {
+                value: value,
+                next: leader.next
+            };
+            leader.next = newNode;
+            this.length++;
         }
         else{
             console.log('yes');
@@ -70,6 +76,36 @@ class LinkedList {
         return this.printList();
 
         
+    }
+
+    //Remove a node from the given index in the linked list
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    remove(index) {
+        const leader = this.traverseToIndex(index);
+        if(leader.next.next !== null) {
+            leader.next = leader.next.next;
+        }
+        else {
+            leader.next = null;
+        }
+        this.length--;
+        return this.printList();
+
+    }
+
+
+    // Traverse to the node which points to the node that is in the index provided
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    traverseToIndex(index) {
+        let currentNode = this.head;
+        for(let i = 0; i < this.length; i++) {
+            if(index == i+1) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
     }
 
 
@@ -81,6 +117,10 @@ myLinkedList.append(20);
 myLinkedList.append(15);
 myLinkedList.prepend(2);
 myLinkedList.insert(2,99);
+console.log(myLinkedList.printList());
+myLinkedList.remove(3);
+console.log(myLinkedList.printList());
+myLinkedList.remove(3);
 console.log(myLinkedList.printList());
 console.log(myLinkedList);
 console.log(myLinkedList.head);
